96da3149f7c840831d0f0df434c28b878d03c5be
Misdinar <55637655+Misdinar@users.noreply.github.com>
2021-05-08 13:37:52 +0800

    semoga ini bener
    wkwkwk tadi salah nangkap maksud soal

--- soal2/soal2a.c
+++ soal2/soal2a.c
@@ -44,13 +44,14 @@ int main(){
     //         printf("%d ", mtx2[i][j]);
     //     }printf("\n");
     // }printf("\n");
+    printf("hasil:\n");
     for(i=0; i < 4;i ++){
         for(j=0; j < 6; j++){
             printf("%d ", value[i*6 + j]);
         }printf("\n");
     }
     while(1){
-		if (*wait == 1) {printf("Lanjot"); break;}
+		if (*wait == 1) {printf("soal2b.c done!\n"); break;}
 		printf("waiting...\n"); sleep(3);
 	}
 
@@ -62,18 +63,18 @@ int main(){
 /*
 mtx 1:
 1 2 3
-4 5 6
-7 8 9
-1 2 3
+3 2 1
+1 0 2
+4 0 1
 
 mtx 2:
-1 2 3 4 5 6
-7 8 9 10 11 12
-1 2 3 4 5 6
+1 2 3 4 2 1
+5 3 2 1 4 0
+0 1 3 1 2 3
 
 hasil:
-18 24 30 36 42 48 
-45 60 75 90 105 120 
-72 96 120 144 168 192 
-18 24 30 36 42 48  
+11 11 16 9 16 10
+13 13 16 15 16 6
+1 4 9 6 6 7
+4 9 15 17 10 7  
 */
