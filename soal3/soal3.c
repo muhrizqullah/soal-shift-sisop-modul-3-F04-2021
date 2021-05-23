1203c096b35cfe9952dd57364cff4f1010a1e780
Muhammad Rizqullah Akbar <muhrizqullah@icloud.com>
2021-05-23 18:48:03 +0700

    fix soal3.c

--- soal3/soal3.c
+++ soal3/soal3.c
@@ -65,7 +65,7 @@ void *categorizeFile(void *arg)
     if (dir)
         closedir(dir);
     else if (ENOENT == errno)
-        mkdir(srcExt, 0666);
+        mkdir(srcExt, 0777);
 
     char *srcName = getName(srcPath);
     char *cwd = getenv("PWD");
